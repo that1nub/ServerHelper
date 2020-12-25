7976dfab628cb372c57264cac42d5a683cdf65b4
Josh <that1nub@gmail.com>
2020-12-25 15:08:18 -0600

    Add files via upload

--- core/functions/webserver.js
+++ core/functions/webserver.js
@@ -1,10 +1,28 @@
-global.encryption = {
-    key: "6pOvVAhTp9pmCBNbgPbgbEC2EwC1zHUh",
-    pass: "Password"
-}
+http.createServer(function(req, res) {        
+    let body = '';
+    req.on('data', function(data) {
+        body += data;
+    });
+    req.on('end', function() {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        if (req.method === "POST") {
+            let posted = {};
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(req.url);
-    res.end();
+            let query = body.split('&');
+            for (let i = 0; i < query.length; i++) {
+                let sides = query[i].split('=');
+                posted[decodeURIComponent(sides[0])] = decodeURIComponent(sides[1]);
+            }
+
+            if (storage.guilds.get(posted['guild_id'])) {
+                res.write("Success!");
+                syncGuild(posted['guild_id']);
+            } else {
+                res.write("Invalid Guild ID: not on guild or guild doesn't exist")
+            }
+        } else {
+            res.write("You must use method POST")
+        }
+        res.end();
+    });
 }).listen(42069);
